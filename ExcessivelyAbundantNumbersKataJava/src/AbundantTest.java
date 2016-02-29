e6fc35e8629228af85d753f6dd20ff2e57781a08
Greig <gm14793@Greig-McAllisters-MacBook-Pro.local>
2016-02-29 12:38:53 -0800

    first solution iteration

---
+++ ExcessivelyAbundantNumbersKataJava/src/AbundantTest.java
@@ -0,0 +1,18 @@
+
+public class AbundantTest {
+
+	public static void main(String[] args){
+		AbundantNumbers test = new AbundantNumbers();
+		System.out.println(testAbundant(test, 12));
+		System.out.println(testAbundant(test, 10));
+		
+	}
+	
+	
+	public static String testAbundant(AbundantNumbers tester, int numToCheck){
+		if(tester.abundantNumber(numToCheck)){
+			return (numToCheck + " is an Abundant Number");
+		}
+		return (numToCheck + " is not an Abundant Number");
+	}
+}
