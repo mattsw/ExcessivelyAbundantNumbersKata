a77696ccbfe6f9f526b998510a489805c10900f6
Matt Wilhelm <mattw@stcu.org>
2016-02-29 10:32:41 -0800

    Initial implementation

---
+++ ExcessivelyAbundantNumbersKata.Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ExcessivelyAbundantNumbersKata.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ProgramTests
+    {
+        [TestFixture]
+        public class IsExcessiveNumber
+        {
+            private Func<int, bool> methodToTest;
+
+            [TestFixtureSetUp]
+            public void Setup()
+            {
+                methodToTest = Program.IsExcessiveNumber;
+            }
+
+            [TestCase(12)]
+            [TestCase(18)]
+            public void ExcessiveNumber_ShouldReturnTrue(int numberToTest)
+            {
+                Assert.IsTrue(methodToTest(numberToTest));
+            }
+
+            [TestCase(11)]
+            [TestCase(13)]
+            public void ExcessiveNumber_ShouldReturnFalse(int numberToTest)
+            {
+                Assert.IsFalse(methodToTest(numberToTest));
+            }
+        }
+    }
+}
---
+++ ExcessivelyAbundantNumbersKata/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExcessivelyAbundantNumbersKata
+{
+    using System;
+    using System.Linq;
+    /*
+    SRC: (http://www.codewars.com/kata/56a75b91688b49ad94000015)
+
+        An abundant number or excessive number is a number for which the sum of its 
+        proper divisors is greater than the number itself.
+        The integer 12 is the first abundant number. 
+        Its proper divisors are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+        Derive function abundantNumber(num)/abundant_number(num) which returns true/True if num is abundant, 
+        false/False if not.
+    */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsExcessiveNumber(12));
+        }
+
+        public static bool IsExcessiveNumber(int numberToCheck)
+        {
+            return Enumerable.Range(1, numberToCheck / 2)
+                .Sum(number => numberToCheck % number == 0 ? number : 0) > numberToCheck;
+        }
+    }
+}
